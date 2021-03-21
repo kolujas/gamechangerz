3f64c720c1ac49025630e86a7e1d93a0fc822891
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-03-20 22:08:27 -0300

    "Controller does not exist" error fixed

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,18 @@
     use Illuminate\Support\Facades\Route;
     
 // * DefaultController - Controls the web in general.
-    Route::get('/', 'Controller@index')->name('web.index');
-    Route::get('/checkout', 'DefaultController@checkout')->name('web.checkout');
-    Route::get('/coming-soon', 'DefaultController@comingSoon')->name('web.coming_soon');
-    Route::get('/game/{slug}', 'DefaultController@game')->name('web.game');
-    Route::get('/home', 'DefaultController@home')->name('web.home');
-    Route::get('/privacy-politics', 'DefaultController@privacyPolitics')->name('web.privacy_politics');
-    Route::get('/terms-&-conditions', 'DefaultController@termsAndConditions')->name('web.terms_&_conditions');
+    Route::get('/', 'App\Http\Controllers\Controller@index')->name('web.index');
+    Route::get('/checkout', 'App\Http\Controllers\DefaultController@checkout')->name('web.checkout');
+    Route::get('/coming-soon', 'App\Http\Controllers\DefaultController@comingSoon')->name('web.coming_soon');
+    Route::get('/game/{slug}', 'App\Http\Controllers\DefaultController@game')->name('web.game');
+    Route::get('/home', 'App\Http\Controllers\DefaultController@home')->name('web.home');
+    Route::get('/privacy-politics', 'App\Http\Controllers\DefaultController@privacyPolitics')->name('web.privacy_politics');
+    Route::get('/terms-&-conditions', 'App\Http\Controllers\DefaultController@termsAndConditions')->name('web.terms_&_conditions');
 
 // * BlogController - Controls the Blog pages.
-    Route::get('/blog', 'BlogController@list')->name('blog.list');
-    Route::get('/blog/{slug}', 'BlogController@details')->name('blog.details');
+    Route::get('/blog', 'App\Http\Controllers\BlogController@list')->name('blog.list');
+    Route::get('/blog/{slug}', 'App\Http\Controllers\BlogController@details')->name('blog.details');
 
 // * UserController - Controls the User pages.
-    Route::get('/{slug}/profile', 'UserController@profile')->name('user.profile');
-    Route::get('/search', 'UserController@search')->name('user.search');
+    Route::get('/{slug}/profile', 'App\Http\Controllers\UserController@profile')->name('user.profile');
+    Route::get('/search', 'App\Http\Controllers\UserController@search')->name('user.search');
