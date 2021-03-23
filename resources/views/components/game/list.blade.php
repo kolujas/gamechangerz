cc91f5ffdabf9629d6ca98dab4e79bcfcc74136c
kolujAs <fernandodeibe@gmail.com>
2021-03-23 19:40:50 -0300

    landing games list

---
+++ resources/views/components/game/list.blade.php
@@ -0,0 +1,42 @@
+<ul class="cards games mt-16 grid lg:grid-cols-4 main">
+    <li style="--game-color-one: #FBBF24; --game-color-two: #D97706;" class="card text-center bg-black pt-4">
+        <header class="pb-4">
+            <h3 class="font-bold text-xl degradado">Counter Strike: GO</h3>
+        </header>
+        <main class="card-body degradado">
+            <figure class="csgo-figure"> {{-- clase unica para el figure img del cs --}}
+                <img class="card-img" src="/../../img/bg-card-csgo.png" alt="Counter Strike Global Offensive">
+            </figure>
+        </main>
+    </li>
+    <li style="--game-color-one: cyan; --game-color-two: blue;" class="card text-center bg-black pt-4 disabled">
+        <header class="pb-4">
+            <h3 class="font-bold text-xl text-gray-100">League of legends</h3>
+        </header>
+        <main class="card-body degradado">
+            <figure>
+                <img class="card-img" src="/../../img/bg-card-lol.png" alt="Counter Strike Global Offensive">
+            </figure>
+        </main>
+    </li>
+    <li style="--game-color-one: red; --game-color-two: maroon;" class="card text-center bg-black pt-4 disabled">
+        <header class="pb-4">
+            <h3 class="font-bold text-xl text-gray-100">Apex Legends</h3>
+        </header>
+        <main class="card-body degradado">
+            <figure>
+                <img class="card-img" src="/../../img/bg-card-apex-legends.png" alt="Apex Legends">
+            </figure>
+        </main>
+    </li>
+    <li style="--game-color-one: yellow; --game-color-two: gold;" class="card text-center bg-black pt-4 disabled">
+        <header class="pb-4">
+            <h3 class="font-bold text-xl text-gray-100">Overwatch</h3>
+        </header>
+        <main class="card-body degradado">
+            <figure>
+                <img class="card-img" src="/../../img/bg-card-overwatch.png" alt="Overwatch">
+            </figure>
+        </main>
+    </li>
+</ul>
