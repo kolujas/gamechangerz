01b44c9b0bde9b81ef4e93580824da5f107362cd
kolujAs <fernandodeibe@gmail.com>
2021-08-06 19:50:34 -0300

    mails 2.0

--- resources/views/mail/friendship-request.blade.php
+++ resources/views/mail/friendship-request.blade.php
@@ -14,7 +14,7 @@
                 <td style="background-color: #281B2D;">
                     <div style="color: #34495e; margin: 4% 10% 2%; text-align: justify;font-family: sans-serif">
                         <h2 style="text-align: center; color: #ED6744;margin: 20px 0;">Nueva solicitud de amistad de {{ $data->username }} ({{ $data->name }})</h2>
-                        <div style="width: 100%; text-align: center; margin-bottom: 4rem 0;">
+                        <div style="width: 100%; text-align: center; margin: 2rem 0;">
                             <a style="font-family: sans-serif;text-decoration:none;border-radius:5px;padding:11px 23px;color:white;background-color: #0D0D0D;"target="_blank" href="http://127.0.0.1:8000/users/{{ $data->slug }}/profile">Revisar perfíl</a>
                         </div>
                     </div>
