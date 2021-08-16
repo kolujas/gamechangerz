d1b5ed66d7ebc7b8f0fc3ec63491227d1d0d48b4
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-08-16 15:25:33 -0300

    Testing with the Event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -30,8 +30,6 @@
          * @return [type]
          */
         public function create () {
-            dd($this->name);
-
             // * Create the Google Event
             $this->event = new GoogleEvent;
 
