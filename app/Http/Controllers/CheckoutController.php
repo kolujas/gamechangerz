18da138ca7899f2fcdc0360a161bf284120265ef
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-08-16 15:22:47 -0300

    Event error fixed

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -86,12 +86,12 @@
             // * Create the GoogleCalendar event.
             if ($lesson->type->id_type === 1 || $lesson->type->id_type === 3) {
                 foreach ($days as $day) {
-                    $data = (object) [];
-                    $data->users = $lesson->users;
-                    $data->name = ($lesson->type->id_type === 3 ? "4 Clases" : "1 Clase") . ($lesson->type->id_type === 2 ? " Offline" : " Online") . " de " . $data->users->from->username;
-                    $data->description = "Clase reservada desde el sitio web GameChangerZ";
-                    $data->started_at = new Carbon($day["date"]."T".Hour::option($day["hour"]["id_hour"])->from);
-                    $data->ended_at = new Carbon($day["date"]."T".Hour::option($day["hour"]["id_hour"])->to);
+                    $data = [];
+                    $data["users"] = $lesson->users;
+                    $data["name"] = ($lesson->type->id_type === 3 ? "4 Clases" : "1 Clase") . ($lesson->type->id_type === 2 ? " Offline" : " Online") . " de " . $lesson->users->from->username;
+                    $data["description"] = "Clase reservada desde el sitio web GameChangerZ";
+                    $data["started_at"] = new Carbon($day["date"]."T".Hour::option($day["hour"]["id_hour"])->from);
+                    $data["ended_at"] = new Carbon($day["date"]."T".Hour::option($day["hour"]["id_hour"])->to);
     
                     new Event($data);
                 }
