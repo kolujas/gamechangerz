09edbd312777b89af58cf3acd01ce5903e69f827
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-08-16 17:52:51 -0300

    quickstart updated

--- quickstart.php
+++ quickstart.php
@@ -22,7 +22,7 @@
         // The file token.json stores the user's access and refresh tokens, and is
         // created automatically when the authorization flow completes for the first
         // time.
-        $tokenPath = './storage/app/google-calendar/oauth-token.json';
+        $tokenPath = 'token.json';
         if (file_exists($tokenPath)) {
             $accessToken = json_decode(file_get_contents($tokenPath), true);
             $client->setAccessToken($accessToken);
