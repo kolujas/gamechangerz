5db8ea64a94cca67a31a1995c9bcfd24c13f07c7
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-08-16 18:00:21 -0300

    quickstart updated

--- quickstart.php
+++ quickstart.php
@@ -29,11 +29,11 @@
         }
 
         // If there is no previous token or it's expired.
-        if ($client->isAccessTokenExpired()) {
+        // if ($client->isAccessTokenExpired()) {
             // Refresh the token if possible, else fetch a new one.
-            if ($client->getRefreshToken()) {
-                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            } else {
+            // if ($client->getRefreshToken()) {
+            //     $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            // } else {
                 // Request authorization from the user.
                 $authUrl = $client->createAuthUrl();
                 printf("Open the following link in your browser:\n%s\n", $authUrl);
@@ -48,13 +48,13 @@
                 if (array_key_exists('error', $accessToken)) {
                     throw new Exception(join(', ', $accessToken));
                 }
-            }
+            // }
             // Save the token to a file.
             if (!file_exists(dirname($tokenPath))) {
                 mkdir(dirname($tokenPath), 0700, true);
             }
             file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-        }
+        // }
         return $client;
     }
 
@@ -66,10 +66,10 @@
     // Print the next 10 events on the user's calendar.
     $calendarId = 'primary';
     $optParams = array(
-    'maxResults' => 10,
-    'orderBy' => 'startTime',
-    'singleEvents' => true,
-    'timeMin' => date('c'),
+        'maxResults' => 10,
+        'orderBy' => 'startTime',
+        'singleEvents' => true,
+        'timeMin' => date('c'),
     );
     $results = $service->events->listEvents($calendarId, $optParams);
     $events = $results->getItems();
