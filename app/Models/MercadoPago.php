9a046457259d12d181e562cd30dbaf245848d58c
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-08-24 23:59:56 -0300

    Testing MercadoPago notification url

--- app/Models/MercadoPago.php
+++ app/Models/MercadoPago.php
@@ -18,6 +18,8 @@
         public function __construct (array $attributes = []) {
             parent::__construct();
 
+            dd($attributes["access_token"]);
+
             // * Set the MercadoPago access token
             SDK::setAccessToken($attributes["access_token"]);
         }
