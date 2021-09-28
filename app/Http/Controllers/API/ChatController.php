10ece26f6a933b66aa96bd5e1a96d80cf114df1b
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-09-28 16:59:58 -0300

    Test chat error

--- app/Http/Controllers/API/ChatController.php
+++ app/Http/Controllers/API/ChatController.php
@@ -50,8 +50,6 @@
                 }
             }
 
-            dd($lessons);
-
             $friends = collect();
             foreach (Friend::allFromUser($request->user()->id_user) as $friend) {
                 $friends->push($friend);
@@ -106,6 +104,8 @@
             foreach ($chats->sortByDesc("updated_at") as $chat) {
                 $sorted->push($chat);
             };
+
+            dd($lessons);
 
             return response()->json([
                 "code" => 200,
