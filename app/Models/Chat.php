96ca8586af6d5c05cae87e27f1f34b51e438f193
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2022-04-18 13:35:07 -0300

    Teting Chat error

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -62,7 +62,7 @@
     
                 return 2;
             } catch (\Throwable $th) {
-                dd($this->attributes['id_user_from']);
+                dd($this);
             }
         }
 
